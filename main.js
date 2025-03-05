8bc27269963a61a23b74a0f9c889159bb01722fb
JasonParzival <ruslanmuhamed123@gmail.com>
2025-03-05 17:27:43 +0800

    Задание выполнил

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 function onButtonClick(btn) {
-    let name = document.getElementById("name"); // получили поле для ввода по id
+    let name = document.getElementById("name"); 
     let newRow = document.getElementById("newRow"); 
+    let quantity = document.getElementById("quantity"); 
 
-    let col = document.createElement("div")
+    let col = document.createElement("div");
     col.innerText = `Я ${name.value}`; 
-    col.className = "col p-2 m-2 bg-warning"
+    col.className = `col-${quantity.value} p-2 m-2 bg-warning`;
 
     newRow.appendChild(col)
 }
